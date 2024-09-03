4853bbbe3ea0e36778abf28c973422a78b0d539f
CelesteHendry <120332026+CelesteHendry@users.noreply.github.com>
2024-09-03 11:25:43 -0400

    button additions

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ import { Card, CardBody } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col items-center justify-center min-h-screen">
       <Navbar>
         <NavbarBrand>
           <User
@@ -45,32 +45,57 @@ export default function Home() {
         </NavbarContent>
       </Navbar>
 
-      <div className="flex justify-center items-center space-x-48 p-4">
-        <Image
-          isZoomed
-          width={350}
-          alt="Portfolio Picture"
-          src="PortfolioPicture.jpg"
-        />
+      <div className="flex flex-col items-center space-y-4 p-4">
+        <div className="flex space-x-4">
+          <Image
+            isZoomed
+            width={350}
+            alt="Portfolio Picture"
+            src="PortfolioPicture.jpg"
+          />
 
-        <Card className="p-4 w-1/4 py-4">
-          <CardBody className=" h-full overflow-visible py-2">
-            <p className="text-tiny uppercase font-bold">Hello!</p>
-            <small className="text-default-500">My name is Celeste</small>
-            <h4 className="font-bold text-large">
-              <p>
-                Ever since I was a kid, I&apos;ve loved playing with computers
-                and video games. They&apos;re like magic boxes that let me
-                explore new worlds and solve puzzles. I&apos;m drawn to the way
-                they blur the lines between reality and fantasy, and how they
-                teach us important life lessons. Now, I&apos;m excited to dive
-                even deeper into computer science, especially game development
-                and AI. I&apos;m thrilled to think about what kind of cool stuff
-                I can create in the future!
-              </p>
-            </h4>
-          </CardBody>
-        </Card>
+          <Card className="p-4 w-1/4 py-4">
+            <CardBody className=" h-full overflow-visible py-2">
+              <p className="text-tiny uppercase font-bold">Hello!</p>
+              <small className="text-default-500">My name is Celeste</small>
+              <h4 className="font-bold text-large">
+                <p>
+                  Ever since I was a kid, I&apos;ve loved playing with computers
+                  and video games. They&apos;re like magic boxes that let me
+                  explore new worlds and solve puzzles. I&apos;m drawn to the
+                  way they blur the lines between reality and fantasy, and how
+                  they teach us important life lessons. Now, I&apos;m excited to
+                  dive even deeper into computer science, especially game
+                  development and AI. I&apos;m thrilled to think about what kind
+                  of cool stuff I can create in the future!
+                </p>
+              </h4>
+            </CardBody>
+          </Card>
+        </div>
+
+        <div className="space-x-4">
+          <Button
+            radius="full"
+            className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
+          >
+            Education
+          </Button>
+
+          <Button
+            radius="full"
+            className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
+          >
+            Projects
+          </Button>
+
+          <Button
+            radius="full"
+            className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
+          >
+            Experience
+          </Button>
+        </div>
       </div>
     </main>
   );
